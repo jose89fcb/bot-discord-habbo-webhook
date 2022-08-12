066908f20e913dd1008a887f6cca286e15dc61e3
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-08-12 05:06:09 +0200

    Add files via upload

---
+++ habbo.py
@@ -0,0 +1,25 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import discord
+from discord.ext import commands
+import time
+
+
+
+bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
+bot.remove_command("help") # Borra el comando por defecto !help
+
+
+@bot.command()
+async def hablar(ctx,   keko, *, mensaje):
+    await ctx.message.delete()
+    await ctx.send("...", delete_after=0)
+    time.sleep(3)
+    
+    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1007483676965220495/TnUp54CPF8nhcYWAVRJW5252Ng51KQYK5X3IeaG5BQfmuzWkXpsb6HxXCVJJtnJOke0C',
+                         username=f"{keko}" ,content=f"{mensaje}", avatar_url=f"https://www.habbo.es/habbo-imaging/avatarimage?user={keko}&action=none&direction=2&head_direction=2&gesture=std&size=l")
+    response = webhook.execute()
+@bot.event
+async def on_ready():
+    print("BOT listo!")
+bot.run("")
+
